4f4a848b437281a97d0ee71a6abe8b9feb7f374f
Will <willev300@gmail.com>
2026-04-14 01:56:06 -0400

    Added Stall and LocateProtocol to efi.h, got gop_interface.

--- bootloader/efi.c
+++ bootloader/efi.c
@@ -3,9 +3,21 @@
 EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable){
     SystemTable->conout->OutputString(
         SystemTable->conout,
-        (uint16_t *)L"Whats up!"
+        (uint16_t *)L"Whats up!\r\n"
     );
 
+    SystemTable->BootServices->Stall(5000000);
+
+    //Get the linear pixel buffer address.
+    EFI_GUID gop_guid = EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID;
+    void *gop_interface = NULL;
+    EFI_STATUS status = SystemTable->BootServices->LocateProtocol(&gop_guid, NULL, &gop_interface);
+
+    if (status == 0) {
+        SystemTable->conout->OutputString(SystemTable->conout, (uint16_t *)L"Found GOP!\r\n");
+    } else {
+        SystemTable->conout->OutputString(SystemTable->conout, (uint16_t *)L"GOP NOT found.\r\n");
+    }
 
     while (1);
 
--- bootloader/efi.h
+++ bootloader/efi.h
@@ -1,27 +1,88 @@
 #include <stdint.h>
 
 #define EFIAPI __attribute__((ms_abi))
+#define EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID \
+{0x9042a9de,0x23dc,0x4a38,\
+{0x96,0xfb,0x7a,0xde,0xd0,0x80,0x51,0x6a}}
 
+//Basic types
 typedef void* EFI_HANDLE;
 typedef uint64_t EFI_STATUS;
+#define NULL ((void *)0)
+typedef unsigned long long UINTN; //Like uint64_t
 
+//GUID type
+typedef struct {
+    uint32_t Data1;
+    uint16_t Data2;
+    uint16_t Data3;
+    uint8_t Data4[8];
+} EFI_GUID;
+
+//Setup for text protocol
 struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
 
+//Function to write text to screen, needs reference to _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL, and string is UTF-16 string to write. Note: UEFI uses \r\n for newline.
 typedef EFI_STATUS (EFIAPI *EFI_TEXT_STRING)(
     struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This,
     uint16_t *String
 );
 
+//The simple text output protocol. Contains the OutputString function.
 typedef struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL {
     void* _unused;
     EFI_TEXT_STRING OutputString; // The function we need
 } EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
 
+//The locate protocol function. Protocol is the GUID, registration is idk, interface is a reference to a pointer for the protocol.
+typedef EFI_STATUS (EFIAPI *EFI_LOCATE_PROTOCOL) (
+    EFI_GUID *Protocol,
+    void *Registration,
+    void **Interface
+);
+
+//The stall method. Like sleep, but microseconds instead.
+typedef EFI_STATUS (EFIAPI *EFI_STALL) (
+    UINTN Microseconds
+);
+
+//The boot services struct.
 typedef struct {
-    char _unused[64];
-    // We'll add the File Protocol and Memory Map pointers here later
+    //Table header
+    char _unused[24];
+
+    //Task Priority Services
+    void *_unused2[2];
+
+    //Memory Services
+    void *_unused3[5];
+
+    //Event & Timer Services
+    void *_unused4[6];
+
+    //Protocol Handler Services
+    void *_unused5[9];
+
+    //Image services
+    void *_unused6[5];
+
+    //Miscellaneous Services
+    void *_unused7;
+    EFI_STALL Stall;
+    void *_unused11;
+
+    //DriverSupport Services
+    void *_unused8[2];
+
+    //Open and Close Protocol Services
+    void *_unused9[3];
+
+    //Library Services
+    void *_unused10[2];
+    EFI_LOCATE_PROTOCOL LocateProtocol;
 } EFI_BOOT_SERVICES;
 
+//The main struct passed to us from UEFI loader.
 typedef struct {
     char _unused[52];
     EFI_HANDLE ConsoleOutHandle;
@@ -32,4 +93,5 @@ typedef struct {
     EFI_BOOT_SERVICES* BootServices;
 } EFI_SYSTEM_TABLE;
 
+//Our entry point.
 EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable);
